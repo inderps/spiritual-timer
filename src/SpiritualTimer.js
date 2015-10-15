93865a4b455fa32fd890daef2f4483ffb7fa3dd4
Inderpal Singh <inderpal@pfeffermind-games.de>
2015-10-15 12:25:24 +0200

    returning actual waiting time

--- src/SpiritualTimer.js
+++ src/SpiritualTimer.js
@@ -1,8 +1,9 @@
 export default class SpiritualTimer {
 
   static setTimeout(method, time) {
+    const startingTime = (new Date()).getTime();
     return setTimeout(() => {
-      method(time);
+      method((new Date()).getTime() - startingTime);
     }, time);
   }
 }
--- test/SpiritualTimer-test.js
+++ test/SpiritualTimer-test.js
@@ -1,9 +1,11 @@
 let chai = require('chai'),
+  MockDate = require('mockdate'),
   SpiritualTimer = require('./../src/SpiritualTimer.js');
 
 chai.should();
 
 describe('setTimeout', () => {
+
   it('should behave same as native setTimeout', (done) => {
     let status = 'Bachelor';
 
@@ -22,8 +24,23 @@ describe('setTimeout', () => {
 
   it('should return the time (ms) it waited', (done) => {
     SpiritualTimer.setTimeout((waitingTime) => {
-      waitingTime.should.equal(200);
+      waitingTime.should.be.at.least(200);
+      waitingTime.should.be.at.most(210);
       done();
     }, 200);
+  });
+
+  it('should return the actual waiting time (ms) if there has been actual delays', (done) => {
+    MockDate.set('1/1/2000 00:00:00');
+    SpiritualTimer.setTimeout((waitingTime) => {
+      waitingTime.should.be.at.least(1000);
+      waitingTime.should.be.at.most(1010);
+      done();
+    }, 200);
+    MockDate.set('1/1/2000 00:00:01');
+  });
+
+  afterEach(() => {
+    MockDate.reset();
   });
 });
